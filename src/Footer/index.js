30366ab101054d4b37ffe5c0c159fb1f714c2515
Ayy OULIDI <ayyoub.oulidi-omali@capgemini.com>
2017-08-07 19:39:54 +0200

    add map

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,4 +1,18 @@
 import React, {Component} from 'react';
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+const Gmap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={13}
+    defaultCenter={{ lat: 34.026929, lng: -4.884638 }}>
+    {props.markers.map((marker, index) => (
+      <Marker
+        key={index}
+        {...marker}/>
+    ))}
+  </GoogleMap>
+));
 
 export default class Footer extends Component {
     render () {
@@ -12,6 +26,10 @@ export default class Footer extends Component {
                             <div className="textwidget"> • Café restau : 0535690718 </div>
                             <div className="textwidget"> • Piscine : 0629239859 / 0666959077 </div>
                         </div>
+                        <Gmap
+                            containerElement={<div style={{ height: `100%` }} />}
+                            mapElement={<div style={{ height: `100%` }} />}
+                            markers={[{position: { lat: 34.026929, lng: -4.884638 }, title: `Menzah Sidi Harazem {lat: 34.026929, lng: -4.884638}`}]}/>
                         <div className="widget-wrapper">			
                             <div className="textwidget"> 
                                 <a href="http://www.github.com/ayyouboulidi" className="link"> Réalisation : Ayyoub oulidi </a>
