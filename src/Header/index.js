85b1e97719edb226638f900227a1bb953f3b5a47
Ayy OULIDI <ayyoub.oulidi-omali@capgemini.com>
2017-08-07 22:13:11 +0200

    update logo

--- src/Header/index.js
+++ src/Header/index.js
@@ -20,9 +20,9 @@ export default class Header extends Component {
         return (
             <div>
                 <div className=" hidden-md-up container">
-                    <div className="row">
+                    <div className="row mt-2">
                         <div className="col-10">
-                            <Link to="/"><img src="/img/logo.png" style={{width: "50px", height:"50px"}} alt="" /></Link>
+                            <Link to="/"><img src="/img/logo.jpeg" style={{width: "70px", height:"70px"}} alt="" /></Link>
                         </div>
                         <div className="col-2 clickable" onClick={this.togglemenu}>
                             <img src="/img/menu.png" style={{width: "50px", height:"50px"}} alt="menu"/>
@@ -75,8 +75,10 @@ export default class Header extends Component {
                         <div className="col-md-1 align-self-center">
                             <Link to="/galerie-photo" className="link"><span>Galerie</span></Link>
                         </div>
-                        <div className="col-md-1 align-self-center">
-                            <Link to="/" className="link"><img src="/img/logo.png" style={{width: "50px", height:"50px"}} alt="" /></Link>
+                        <div className="col-md-2 align-self-center">
+                            <Link to="/" className="link d-flex">
+                                <img src="/img/logo.jpeg" className="align-items-center" style={{width: "70px", height:"70px", display:'block', margin:'auto'}} alt="logo" />
+                            </Link>
                         </div>
                         <div className="col-md-2 align-self-center">
                             <Link to="/la-carte" className="link"><span>Carte</span></Link>
