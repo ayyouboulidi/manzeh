df8b8bd1e1e10afe56a85e79394a9cf697b705ef
Ayy OULIDI <ayyoub.oulidi-omali@capgemini.com>
2017-09-06 01:04:29 +0200

    feat(map): update google map

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -19,20 +19,32 @@ export default class Footer extends Component {
         return (
             <div className="container">
                 <div className="row mt-5">
-                    <div className="col-md-6">
-                        <div className="widget-wrapper">
-                            <h3 className="widget-title">Menzah Sidi Harazem</h3>			
-                            <div className="textwidget">Manzeh Sidi Harazem • Sidi Harazem Fès </div>
-                            <div className="textwidget"> • Café restau : 0535690718 </div>
-                            <div className="textwidget"> • Piscine : 0629239859 / 0666959077 </div>
+                    <div className="col-md-6 col-12">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="widget-wrapper">
+                                    <h3 className="widget-title">Menzah Sidi Harazem</h3>			
+                                    <div className="textwidget">Manzeh Sidi Harazem • Sidi Harazem Fès </div>
+                                    <div className="textwidget"> • Café restau : 0535690718 </div>
+                                    <div className="textwidget"> • Piscine : 0629239859 / 0666959077 </div>
+                                </div>
+                            </div>
                         </div>
-                        <Gmap
-                            containerElement={<div style={{ height: `100%` }} />}
-                            mapElement={<div style={{ height: `100%` }} />}
-                            markers={[{position: { lat: 34.026929, lng: -4.884638 }, title: `Menzah Sidi Harazem {lat: 34.026929, lng: -4.884638}`}]}/>
-                        <div className="widget-wrapper">			
-                            <div className="textwidget"> 
-                                <a href="http://www.github.com/ayyouboulidi" className="link"> Réalisation : Ayyoub oulidi </a>
+                        <div className="row">
+                            <div className="col-12">
+                                <Gmap
+                                containerElement={<div style={{ height: `300px` }} />}
+                                mapElement={<div style={{ height: `100%` }} />}
+                                markers={[{position: { lat: 34.026929, lng: -4.884638 }, title: `Menzah Sidi Harazem {lat: 34.026929, lng: -4.884638}`}]}/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="widget-wrapper">			
+                                    <div className="textwidget"> 
+                                        <a href="http://www.github.com/ayyouboulidi" className="link"> Réalisation : Ayyoub oulidi </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
